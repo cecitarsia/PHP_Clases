6244b35fbf9f4e3b5bf85d9bca51cb53107a8d74
cecitarsia <ceciliatarsia@gmail.com>
2019-06-05 16:55:12 -0300

    agregado carpeta Entregable

---
+++ Clase 9 - Session y Cookies/mostrar.php
@@ -0,0 +1,12 @@
+  <?php
+  $persona = [
+    "nombre" => "Cecilia",
+    "edad" => 31,
+    "Gato" => "Adolfo",
+  ];
+
+  foreach ($persona as $atributo => $value) {
+    echo "$atributo: $value <br>";
+  }
+
+   ?>
